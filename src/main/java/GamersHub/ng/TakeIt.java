09553f8d3e27c7c36ae2b8648380c5ac00472433
jaximuss <chidiumeo@gmail.com>
2022-10-28 22:33:26 +0100

    discord java bot

---
+++ src/main/java/GamersHub/ng/TakeIt.java
@@ -0,0 +1,41 @@
+package GamersHub.ng;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
+public class TakeIt {
+
+    private final Dotenv config;
+    private final ShardManager shardManager;
+
+    public TakeIt()
+    {
+        config = Dotenv.configure().load();
+
+        //lets get put token from config
+            String token =    config.get("TOKEN");
+        //this is like an event hanlder in c#
+        DefaultShardManagerBuilder Builder = DefaultShardManagerBuilder.createDefault(token);
+        Builder.setStatus(OnlineStatus.ONLINE);
+        Builder.setActivity(Activity.playing("deez nutz??"));
+        Builder.addEventListeners();
+        shardManager =  Builder.build();
+
+    }
+
+    public ShardManager getShardManager() {
+        return shardManager;
+    }
+
+    public Dotenv getConfig() {
+            return config;
+    }
+
+    public static void main(String[] args)
+    {
+            TakeIt takeIt = new TakeIt();
+    }
+}
